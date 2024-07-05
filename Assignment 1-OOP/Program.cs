2968ab7044013b30af0bc2df159d8e0d3275bd8c
DiabloXV <haboali1001@gmail.com>
2024-07-05 04:15:02 +0300

    edited

--- Assignment 1-OOP/Program.cs
+++ Assignment 1-OOP/Program.cs
@@ -190,9 +190,30 @@
                 #endregion
 
             }
-        }
+            /*                      FUNCTIONS USED                  */
 
-    }
+            public static double CalculateDistance(Point p1, Point p2)
+            {
+                double deltaX = p2.X - p1.X;
+                double deltaY = p2.Y - p1.Y;
+                return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+            }
+
+            public static Person FindOldestPerson(Person[] people)
+            {
+                Person oldest = people[0];
+                foreach (Person person in people)
+                {
+                    if (person.Age > oldest.Age)
+                    {
+                        oldest = person;
+                    }
+                }
+                return oldest;
+
+            }
+
+        }
 }
 
 
